83b3fcc5ee762422a49371778bc5b6cab2bdbad4
heber.mattos <feanorkun@hotmail.com>
2016-09-06 10:13:08 -0300

    undo

--- NetCircuitBreaker/Properties/AssemblyInfo.cs
+++ NetCircuitBreaker/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CircuitBreaker")]
+[assembly: AssemblyTitle("NetCircuitBreaker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
